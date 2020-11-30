c103b5083ca0b43f9bfda8f6906f3f48fdd3775d
OptiPlex 7070 <a.gulistani@mfa.af>
2020-11-30 15:27:55 +0430

    Todo template added.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="hero" className="hero">
+      <h1 className="title">To Do List</h1>
+      <div id="todoApp" className="todo-app">
+        <div id="todoMenu1" className="todo-menu-1">
+          <button id="toggleAll" className="toggle-all" aria-label="Toggle all to do tasks">
+            <span className="rotate">&#x276F;</span>
+          </button>
+          <input id="addTodoTextInput" className="add-todo-text-input" type="text" placeholder="What do you need to do?" aria-label="Enter to do text" autofocus/>
+        </div>
+        <ul id="todos" className="todos" aria-label="List of to do tasks"></ul>
+        <div id="todoMenu2" className="todo-menu-2">
+          <label id="todosLeft" className="todos-left" aria-label="Number of to do tasks left to complete"></label>
+          <div id="todoMenu2Buttons" className="todo-menu-2-buttons">
+            <button id="showAllTodos" className="menu-2-button active" aria-label="Show all to do tasks">All</button>
+            <button id="showUncompletedTodos" className="menu-2-button" aria-label="Show active to do tasks">Active</button>
+            <button id="showCompletedTodos" className="menu-2-button" aria-label="Show completed to do tasks">Completed</button>
+          </div>
+          <button id="deleteCompletedButton" className="delete-completed-button" aria-label="Clear completed to do tasks">Clear completed</button>
+        </div>
+      </div>
     </div>
   );
 }
